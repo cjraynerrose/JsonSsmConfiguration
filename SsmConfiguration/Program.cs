20fbacf22ea871a1beea96fc32429fd2d84096d9
chas <cjraynerrose@gmail.com>
2020-06-13 12:55:07 +0100

    Add VS solution and dotnet core 3.1 console proj

---
+++ SsmConfiguration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SsmConfiguration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
